dfc3c09d6d53f2af27459ee0af14a5106e441c06
Iury Calado de Araujo <78110326+iury-dearaujo@users.noreply.github.com>
2023-05-13 02:10:23 -0300

    Update rXcelController.js

--- src/api/controllers/rXcelController.js
+++ src/api/controllers/rXcelController.js
@@ -4,20 +4,22 @@ const PATH_XLSX = process.env.PATH_XLSX || './downloads';
 const all = (resquest, response) => {
     const _resquest = resquest.headers;
 
-    console.log(_resquest)
-
     const workbook = xlsx.readFile(`${PATH_XLSX}/yumi.xlsx`);
     const worksheet = workbook.Sheets[workbook.SheetNames[0]]
     const dataJ = worksheet;
 
     const dynamicDate = {
+          "to": "yumi",
+          "from": "iury",
+          "msg": "Yu 🥰 sou apaixonado por você!"
+        };/*{
         "resquest": {
             'user-agent': resquest.headers['user-agent'],
             'host': resquest.headers['host']
         },
         "file_info": dataJ['!ref'].split(":"),
         "file": dataJ
-    };
+    };*/
     response.status(200).send(dynamicDate);
 };
 
