9b410a9737178f596f2c57f1e824fa436fb7b240
Iury Araujo <iury.araujo@senior.com.br>
2023-05-15 16:51:54 -0300

    Update 0.10.0

--- src/api/controllers/yumi.js
+++ src/api/controllers/yumi.js
@@ -4,7 +4,8 @@ const all = (resquest, response) => {
     const _resquest_H = resquest.headers;
 
     response.status(200)
-    response.send("I love you!")
+    response.send("I love you! <3<3<3")
+
 }
 
 module.exports = {
